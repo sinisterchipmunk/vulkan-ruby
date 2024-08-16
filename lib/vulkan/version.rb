54ae270491e093f67599dc7a5882a46906d894f8
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2024-08-16 13:13:17 -0400

    bump version

--- lib/vulkan/version.rb
+++ lib/vulkan/version.rb
@@ -1,7 +1,7 @@
 require 'vulkan/generated/version'
 
 module Vulkan
-  VULKAN_RUBY_VERSION = [ 4 ]
+  VULKAN_RUBY_VERSION = [ 5 ]
 
   VERSION = [ VK_API_VERSION_MAJOR,
               VK_API_VERSION_MINOR,
