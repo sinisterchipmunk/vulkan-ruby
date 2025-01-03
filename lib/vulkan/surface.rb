166427c103a535b86c9f9eeea41d5c54ed646d6f
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2025-01-03 10:38:17 -0500

    keep reference to instance

--- lib/vulkan/surface.rb
+++ lib/vulkan/surface.rb
@@ -3,6 +3,7 @@ module Vulkan
     include Vulkan::Finalizer
     
     def initialize(instance, handle)
+      @instance = instance
       @handle = handle
       finalize_with Vulkan[instance, nil], :vkDestroySurfaceKHR, instance.to_ptr, @handle, nil
     end
